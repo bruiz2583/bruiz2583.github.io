9b4da249e53f3e01cd5db65ee66367aa269e5a52
Bryan Ruiz <bryanruiz@Bryans-MacBook-Pro.local>
2016-03-14 18:47:57 -0500

    tidy up project directory

---
+++ js/javascript.js
@@ -0,0 +1,41 @@
+// var multiplier = 9;
+// for (var i = 0; i <= 10; i++) {
+//     var result = multiplier * i;
+//     console.log(multiplier + ' * ' + i + ' = ' + result);
+// }
+
+
+// for (var multiplier = 0; multiplier <= 10; multiplier++) {
+//  for (var i = 0; i <= 10; i++) {
+//    var result = multiplier * i;
+//    console.log(multiplier + ' * ' + i + ' = ' + result);
+//  }
+// }
+function submitComment() {
+	var name = $("#name").val();
+	var comment = $("#comment").val();
+
+	var data = {
+		name: name,
+		comment: comment 
+	};
+
+	var ajaxInfo = {
+		url: "http://localhost:3000",
+		type: "POST",
+		dataType: "JSON",
+		data: JSON.stringify(data),
+		crossDomain:true,
+		sucess: function() {
+			alert("Sucess!");
+		},
+		error: function (param1, param2, param3) {
+			console.log(param1);
+			console.log(param2);
+			console.log(param3);
+			// alert("Sorry, something went wrong.");	
+		}
+	};
+
+$.ajax(ajaxInfo)
+}
---
+++ js/script.js
@@ -0,0 +1,82 @@
+//var numberofChildren = 5;
+//var partnersName = "Judy";
+//var geographicLocation = "Chicago";
+//var jobTitle = "Teacher";
+
+//console.log("You will be a " + jobTitle + " in " + geographicLocation  + 
+//	" and married to " + partnersName + 
+//" with " + numberofChildren + " kids.");
+
+//var radius = 2;
+//var circumference = 2 * 3.14 * radius;
+//var outputString = "The circumference is " + circumference;
+//console.log(outputString);
+
+//var area = 3.14 * (radius * radius);
+//outputString = "The area is " + area;
+//console.log(outputString);
+
+//var numberofChildren = 5;
+//var partnersName = "Judy";
+//var geographicLocation = "Chicago";
+//var jobTitle = "Teacher";
+
+//function tellFortune (numberofChildren, partnersName, geographicLocation, jobTitle)
+//console.log("You will be a " + jobTitle + " in " + geographicLocation  + 
+//	" and married to " + partnersName + " with " + numberofChildren + " kids.");
+
+//}
+//tellFortune(7, "Judy", "Memphis", "Librarian");
+
+//var radius = 2;
+//var circumference = 2 * 3.14 * radius;
+//var outputString = "The circumference is " + circumference;
+//console.log(outputString);
+
+//var area = 3.14 * (radius * radius);
+//outputString = "The area is " + area;
+//console.log(outputString);
+
+
+//var shouldLoop = true;
+//while(shouldLoop) {
+//	console.log("Hello, I'm an infinite loop.");
+//	shouldLoop = false;
+//}
+
+//var x = 0;
+//	while(x < 5) {
+//	console.log("Hello, I'm an infinite loop.");
+//	x = x + 1;
+//
+//}
+
+//function myName (Friends)
+//var myArray = ("Bryan", "Brandon",)
+
+function countToTen (){
+
+var i = 1;
+	while(i < 11) {
+	console.log(i);
+	i = i + 1;
+}
+}
+
+countToTen()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
